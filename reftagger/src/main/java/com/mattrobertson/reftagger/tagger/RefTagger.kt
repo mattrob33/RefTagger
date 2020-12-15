71c8647269741817e7992b1d3c23349590b36204
Matt Robertson <mattrobertson333@gmail.com>
2020-12-15 10:05:39 -0600

    Added support for stylizing link appearance

--- reftagger/src/main/java/com/mattrobertson/reftagger/tagger/RefTagger.kt
+++ reftagger/src/main/java/com/mattrobertson/reftagger/tagger/RefTagger.kt
@@ -1,11 +1,14 @@
 package com.mattrobertson.reftagger.tagger
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.net.Uri
 import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import android.text.Spanned
+import android.text.TextPaint
 import android.text.style.ClickableSpan
+import android.text.style.StyleSpan
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -17,10 +20,14 @@ import com.mattrobertson.reftagger.utils.createBibleGatewayUrl
 class RefTagger private constructor(builder: Builder) {
 
     private var clickHandler: ClickHandler? = builder.clickHandler
-
     private var handlerType = builder.handlerType
 
     private var defaultVersion = builder.defaultVersion
+
+    private var linkTextColor = builder.linkColor
+    private var underlineLinks = builder.underlineLinks
+    private var italicizeLinks = builder.italicizeLinks
+    private var boldLinks = builder.boldLinks
 
     companion object {
         private val REFERENCE_REGEX = "((?:(Genesis|Gen?|Gn|Exodus|Exod?|Ex|Leviticus|Le?v|Numbers|Nu?m|Nu|Deuteronomy|Deut?|Dt|Josh?ua|Josh?|Jsh|Judges|Ju?dg|Jg|Ru(?:th)?|Ru?t|(?:1|i|2|ii) ?Samuel|(?:1|i|2|ii) ?S(?:a|m)|(?:1|i|2|ii) ?Sam|(?:1|i|2|ii) ?Kin(?:gs?)?|(?:1|i|2|ii) ?Kgs|(?:1|i|2|ii) ?Chronicles|(?:1|i|2|ii) ?Chr(?:o?n)?|(?:1|i|2|ii) ?Cr|Ezra?|Nehemiah|Neh?|Esther|Esth?|Jo?b|Psalms?|Psa?|Proverbs|Pro?v?|Ecclesiastes|Ec(?:cl?)?|Song (?:O|o)f Solomon|Song (?:O|o)f Songs?|Son(?:gs?)?|SS|Isaiah?|Isa?|Jeremiah|Je?r|Lamentations|La(?:me?)?|Ezekiel|Eze?k?|Daniel|Da?n|Da|Hosea|Hos?|Hs|Jo(?:el?)?|Am(?:os?)?|Obadiah|Ob(?:ad?)?|Jon(?:ah?)?|Jnh|Mic(?:ah?)?|Mi|Nah?um|Nah?|Habakkuk|Hab|Zephaniah|Ze?ph?|Haggai|Hagg?|Hg|Zechariah|Ze?ch?|Malachi|Ma?l|Matthew|Matt?|Mt|Mark|Ma(?:r|k)|M(?:r|k)|Luke?|Lk|Lu?c|John|Jn|Ac(?:ts?)?|Romans|Ro?m|(?:1|i|2|ii) ?Corinthians|(?:1|i|2|ii) ?C(?:or?)?|Galatians|Gal?|Gl|Ephesians|Eph?|Philippians|Phil|Colossians|Co?l|(?:1|i|2|ii) ?Thessalonians|(?:1|i|2|ii) ?Th(?:e(?:ss?)?)?|(?:1|i|2|ii) ?Timothy|(?:1|i|2|ii) ?Tim|(?:1|i|2|ii) ?T(?:i|m)|Ti(?:tus)?|Ti?t|Philemon|Phl?m|Hebrews|Heb?|Jam(?:es)?|Jms|Jas|(?:1|i|2|ii) ?Peter|(?:1|i|2|ii) ?Pe?t?|(?:1|i|2|ii|3|iii) ?J(?:oh)?n?|Jude?|Revelations?|Rev|R(?:e|v))(?:.)? *?)?(?:(\\d*):)?(\\d+(?:(?:ff|f|\\w)|(?:\\s?(?:-|–|—)\\s?\\d+)?)))([^a-z0-9]*)".toRegex()
@@ -29,7 +36,13 @@ class RefTagger private constructor(builder: Builder) {
     class Builder {
         internal var clickHandler: ClickHandler? = null
         internal var handlerType = HandlerType.DIALOG
+
         internal var defaultVersion = "NIV"
+
+        internal var linkColor = -1 // use default link color unless one is specified
+        internal var underlineLinks = true
+        internal var italicizeLinks = false
+        internal var boldLinks = false
 
         fun useDialog(): Builder {
             handlerType = HandlerType.DIALOG
@@ -54,6 +67,26 @@ class RefTagger private constructor(builder: Builder) {
 
         fun setDefaultVersion(version: String): Builder {
             defaultVersion = version
+            return this
+        }
+
+        fun setLinkColor(color: Int): Builder {
+            linkColor = color
+            return this
+        }
+
+        fun underlineLinks(underline: Boolean): Builder {
+            underlineLinks = underline
+            return this
+        }
+
+        fun italicizeLinks(italicize: Boolean): Builder {
+            italicizeLinks = italicize
+            return this
+        }
+
+        fun boldLinks(bold: Boolean): Builder {
+            boldLinks = bold
             return this
         }
 
@@ -112,10 +145,27 @@ class RefTagger private constructor(builder: Builder) {
             override fun onClick(view: View) {
                 onClick(ref, view)
             }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+
+                ds.apply {
+                    isUnderlineText = underlineLinks
+                    if (linkTextColor != -1)
+                        color = linkTextColor
+                }
+            }
         }
 
         return SpannableString(ref).apply {
             setSpan(link, 0, to - from + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            if (italicizeLinks && boldLinks)
+                setSpan(StyleSpan(Typeface.BOLD_ITALIC), 0, to - from + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            else if (italicizeLinks)
+                setSpan(StyleSpan(Typeface.ITALIC), 0, to - from + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            else if (boldLinks)
+                setSpan(StyleSpan(Typeface.BOLD), 0, to - from + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         }
     }
 
